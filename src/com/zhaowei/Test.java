f7c518f38210bc824558f3f5cc709d2d28f5b164
zhaovgs <128895836+zhaovgs@users.noreply.github.com>
2023-03-25 18:24:49 +0800

    up
    up

---
+++ src/com/zhaowei/Test.java
@@ -0,0 +1,13 @@
+package com.zhaowei;
+
+/**
+ * @Description :
+ * @Author : zhaowei
+ * @Date: 2023/3/25  17:05
+ * @Version:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello data structure");
+    }
+}
